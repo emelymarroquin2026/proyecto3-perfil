dd79ac04dbc4104a002de56f59e19211d2911db1
Georgina <emely.marroquin2026@adoc.superate.org.sv>
2026-09-25 10:34:11 -0600

    feat: estructura base para los pilotos

---
+++ app.js
@@ -0,0 +1,13 @@
+// =========================================
+// PROYECTO 3 - JAVASCRIPT INICIAL
+// =========================================
+
+// Seleccionamos el botón
+const botonEstadisticas = document.getElementById("btn-estadisticas");
+
+// Seleccionamos el espacio donde aparecerán
+// las estadísticas
+const estadisticas = document.getElementById("estadisticas");
+
+// Por ahora el botón no realiza ninguna acción.
+// La funcionalidad será agregada en la Iteración 3.1.
