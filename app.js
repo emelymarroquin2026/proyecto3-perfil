be35aca373960486eb0422bd6cb111770e0d8515
AndreasRivas <andrea.rivas2026@adoc.superate.org>
2026-09-25 10:46:16 -0600

    Mostrar estadistica con js

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 // =========================================
-// PROYECTO 3 - JAVASCRIPT INICIAL
+
+// ITERACIÓN 3.1
+
 // =========================================
-
+ 
 // Seleccionamos el botón
+
 const botonEstadisticas = document.getElementById("btn-estadisticas");
+ 
+// Seleccionamos el espacio de estadísticas
 
-// Seleccionamos el espacio donde aparecerán
-// las estadísticas
 const estadisticas = document.getElementById("estadisticas");
+ 
+ 
+// Al hacer clic en el botón,
 
-// Por ahora el botón no realiza ninguna acción.
-// La funcionalidad será agregada en la Iteración 3.1.
+// se muestran las estadísticas
+ 
+botonEstadisticas.addEventListener("click", function () {
+ 
+    estadisticas.textContent = "Carreras: 10 | Puntos: 250";
+ 
+});
+ 
